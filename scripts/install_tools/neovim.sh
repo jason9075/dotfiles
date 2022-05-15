0e1feeca8a68e6ae49f3dc723c96af144ce8e40e
Jason Kuan <jason9075@gmail.com>
2022-05-15 12:10:30 +0000

    hotfix: curl wrong arg

--- scripts/install_tools/neovim.sh
+++ scripts/install_tools/neovim.sh
@@ -3,8 +3,8 @@
 set -e
 
 install_linux() {
-  mkdir -p "~/.local/bin"
-  curl -LO "https://github.com/neovim/neovim/releases/latest/download/nvim.appimage" -o "${HOME}/.local/bin/nvim"
+  mkdir -p "${HOME}/.local/bin"
+  curl -L "https://github.com/neovim/neovim/releases/latest/download/nvim.appimage" -o "${HOME}/.local/bin/nvim"
   chmod u+x "${HOME}/.local/bin/nvim"
 }
 
