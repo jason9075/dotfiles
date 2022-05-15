71aec29c935cb69601021152cb8ba953251ac4bc
Jason Kuan <jason9075@gmail.com>
2022-05-15 13:22:13 +0000

    update add tpm fnm node for neovim plugin dependency

---
+++ scripts/install_steps/4-fnm.sh
@@ -0,0 +1,12 @@
+echo 'installing fnm...'
+
+if [ $(uname -r | grep -i ish) ]; then
+  echo 'fnm does not work on ish currently, skip installation.'
+  exit 0
+fi
+
+if type brew &> /dev/null; then
+  brew install fnm
+else
+  curl -fsSL https://fnm.vercel.app/install | bash -s -- --install-dir "$HOME/.local/share/fnm" --skip-shell
+fi
