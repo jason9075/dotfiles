53c1ab767f0e2fbebcb79763b90b9dd67efbb73d
jason9075 <jason9075@users.noreply.github.com>
2022-05-16 22:08:54 +0800

    hotfix:use miniconda3 as python 3.x

--- scripts/install_tools/miniconda.sh
+++ scripts/install_tools/miniconda.sh
@@ -6,12 +6,12 @@ if [ -d "$HOME/miniconda3" ]; then
 fi
 
 if type apt-get &> /dev/null; then
-  curl -sL "https://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh" -o ~/Miniconda_Install.sh
+  curl -sL "https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh" -o ~/Miniconda_Install.sh
   chmod u+x ~/Miniconda_Install.sh
   bash ~/Miniconda_Install.sh -b -p ~/miniconda3 
   rm ~/Miniconda_Install.sh
 elif type brew &> /dev/null; then
-  curl -sL "https://repo.continuum.io/miniconda/Miniconda-latest-MacOSX-x86_64.sh" -o ~/Miniconda_Install.sh
+  curl -sL "https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh" -o ~/Miniconda_Install.sh
   chmod u+x ~/Miniconda_Install.sh
   bash ~/Miniconda_Install.sh -b -p ~/miniconda3 
   rm ~/Miniconda_Install.sh
