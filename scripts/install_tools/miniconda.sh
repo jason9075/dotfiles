05c0004125ce3b63207cc0ac57f0ad61005fac95
jason9075 <jason9075@users.noreply.github.com>
2022-05-16 22:02:46 +0800

    hotfix: add chmod to miniconda

--- scripts/install_tools/miniconda.sh
+++ scripts/install_tools/miniconda.sh
@@ -6,12 +6,14 @@ if [ -d "$HOME/miniconda3" ]; then
 fi
 
 if type apt-get &> /dev/null; then
-  curl "https://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh" -o ~/Miniconda_Install.sh
-  ~/Miniconda_Install.sh -b -p ~/miniconda3 
+  curl -sL "https://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh" -o ~/Miniconda_Install.sh
+  chmod u+x ~/Miniconda_Install.sh
+  bash ~/Miniconda_Install.sh -b -p ~/miniconda3 
   rm ~/Miniconda_Install.sh
 elif type brew &> /dev/null; then
-  curl "https://repo.continuum.io/miniconda/Miniconda-latest-MacOSX-x86_64.sh" -o ~/Miniconda_Install.sh
-  ~/Miniconda_Install.sh -b -p ~/miniconda3 
+  curl -sL "https://repo.continuum.io/miniconda/Miniconda-latest-MacOSX-x86_64.sh" -o ~/Miniconda_Install.sh
+  chmod u+x ~/Miniconda_Install.sh
+  bash ~/Miniconda_Install.sh -b -p ~/miniconda3 
   rm ~/Miniconda_Install.sh
 fi
 
