ae7dfda6e03ed2b692d45739b62f12299f55dfc1
jason9075 <jason9075@users.noreply.github.com>
2022-05-20 15:05:26 +0800

    update: add cpp lsp support

---
+++ nvim/docs/cpp.md
@@ -0,0 +1,10 @@
+# third party not found
+
+Add cmake flag CMAKE_EXPORT_COMPILE_COMMANDS=1 to solve.
+```
+mkdir build
+cd build
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ..
+```
+This [dude's video](https://www.youtube.com/watch?v=BCuyEdDQ5iA) help me a lot.
+
--- nvim/lua/user/lsp/lsp-installer.lua
+++ nvim/lua/user/lsp/lsp-installer.lua
@@ -34,6 +34,10 @@ lsp_installer.on_server_ready(function(server)
         local pyright_opts = require("user.lsp.settings.golangci_lint_ls")
         opts = vim.tbl_deep_extend("force", pyright_opts, opts)
     end
+    if server.name == "clangd" then
+        local clangd = require("user.lsp.settings.clangd")
+        opts = vim.tbl_deep_extend("force", clangd, opts)
+    end
     -- This setup() function is exactly the same as lspconfig's setup function.
     -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
     server:setup(opts)
