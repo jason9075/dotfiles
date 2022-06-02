0d7c3944d85323a9eea854f56ce5c7dab8a8fcaa
jason9075 <jason9075@users.noreply.github.com>
2022-06-02 08:21:46 +0800

    optimize: reduce python3 provider loading time

--- nvim/lua/user/dap.lua
+++ nvim/lua/user/dap.lua
@@ -9,26 +9,31 @@ if not dap_ui_status_ok then
 end
 
 -- Python
-if vim.fn.has("python3") == 1 then
-    -- Reference: https://github.com/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation#Python
-    local python_path = function ()
-        local cwd = vim.fn.getcwd()
-        if os.getenv("CONDA_PREFIX") ~= nil then
-            return os.getenv("CONDA_PREFIX") .. '/bin/python'
-        elseif os.getenv("CONDA_PYTHON_EXE") ~= nil then
-            return os.getenv("CONDA_PYTHON_EXE")
-        elseif vim.fn.executable(cwd .. '/venv/bin/python') == 1 then
-            return cwd .. '/venv/bin/python'
-        elseif vim.fn.executable(cwd .. '/.venv/bin/python') == 1 then
-            return cwd .. '/.venv/bin/python'
-        else
-            return '/usr/bin/python3'
-        end
+
+-- Reference: https://github.com/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation#Python
+local get_python_path = function ()
+    local cwd = vim.fn.getcwd()
+    if os.getenv("CONDA_PREFIX") ~= nil then
+        return os.getenv("CONDA_PREFIX") .. '/bin/python'
+    elseif os.getenv("CONDA_PYTHON_EXE") ~= nil then
+        return os.getenv("CONDA_PYTHON_EXE")
+    elseif vim.fn.executable(cwd .. '/venv/bin/python') == 1 then
+        return cwd .. '/venv/bin/python'
+    elseif vim.fn.executable(cwd .. '/.venv/bin/python') == 1 then
+        return cwd .. '/.venv/bin/python'
+    elseif vim.fn.executable('/usr/bin/python3') == 1 then
+        return '/usr/bin/python3'
+    else
+        return nil
     end
+end
+
+python_path = get_python_path()
+if python_path ~= nil then
 
     dap.adapters.python = {
         type = 'executable';
-        command = python_path();
+        command = get_python_path();
         args = { '-m', 'debugpy.adapter' };
     }
 
@@ -46,10 +51,11 @@ if vim.fn.has("python3") == 1 then
         },
     }
     
-    vim.g.python3_host_prog = python_path()
+    vim.g.python3_host_prog = python_path
 end
 
 -- C++
+
 -- download: https://github.com/vadimcn/vscode-lldb/releases
 -- unzip
 -- unzip codelldb-x86_64-linux.vsix -d ~/codelldb/
