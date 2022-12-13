49fceaab6d23e786935288b984a2124c42f905e4
Jason Kuan <jason9075@users.noreply.github.com>
2022-12-13 22:46:25 +0800

    hotfix typo

--- nvim/lua/user/lsp/lsp-installer.lua
+++ nvim/lua/user/lsp/lsp-installer.lua
@@ -24,8 +24,8 @@ lsp_installer.on_server_ready(function(server)
         local gopls_opts = require("user.lsp.settings.gopls")
         opts = vim.tbl_deep_extend("force", gopls_opts, opts)
     elseif server.name == "golangci_lint_ls" then
-        local pyright_opts = require("user.lsp.settings.golangci_lint_ls")
-        opts = vim.tbl_deep_extend("force", pyright_opts, opts)
+        local golangci_opts = require("user.lsp.settings.golangci_lint_ls")
+        opts = vim.tbl_deep_extend("force", golangci_opts, opts)
     elseif server.name == "clangd" then
         local clangd = require("user.lsp.settings.clangd")
         opts = vim.tbl_deep_extend("force", clangd, opts)
