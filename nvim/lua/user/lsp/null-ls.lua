b4b56060fa3280936b5ece16eec19144302e0a0e
Jason Kuan <jason9075@users.noreply.github.com>
2022-12-26 21:29:11 +0800

    update: add snippets

--- nvim/lua/user/lsp/null-ls.lua
+++ nvim/lua/user/lsp/null-ls.lua
@@ -8,6 +8,8 @@ local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
 
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 null_ls.setup {
     debug = false,
     sources = {
@@ -18,14 +20,17 @@ null_ls.setup {
         diagnostics.flake8.with { extra_args = { "--max-line-length", "120" } },
         diagnostics.staticcheck,
     },
-    on_attach = function(client)
-        if client.server_capabilities.document_formatting then
-            vim.cmd([[
-            augroup LspFormatting
-                autocmd! * <buffer>
-                autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
-            augroup END
-            ]])
+    on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                group = augroup,
+                buffer = bufnr,
+                callback = function()
+                    -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+                    vim.lsp.buf.format({ bufnr = bufnr })
+                end,
+            })
         end
     end,
 }
--- nvim/lua/user/lsp/settings/terraformls.lua
+++ nvim/lua/user/lsp/settings/terraformls.lua
@@ -1,7 +1,7 @@
 -- Usage: https://github.com/hashicorp/terraform-ls/blob/main/docs/USAGE.md
 vim.api.nvim_create_autocmd({"BufWritePre"}, {
   pattern = {"*.tf", "*.tfvars"},
-  callback = vim.lsp.buf.formatting_sync,
+  callback = vim.lsp.buf.format,
 })
 
 return {}
