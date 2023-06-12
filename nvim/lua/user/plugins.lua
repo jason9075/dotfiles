caa25b3a4042d60e79d1b90be65bad858ab7be37
jason9075 <jason9075@users.noreply.github.com>
2023-06-12 17:46:27 +0800

    update packer to lazy.nvim

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -1,71 +1,40 @@
-local fn = vim.fn
-
--- Automatically install packer
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system({
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
     "git",
     "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
   })
-  print("Installing packer close and reopen Neovim...")
-  vim.cmd([[packadd packer.nvim]])
 end
+vim.opt.rtp:prepend(lazypath)
 
--- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]])
+local plugins = {
 
--- Use a protected call so we don't error out on first use
-local status_ok, packer = pcall(require, "packer")
-if not status_ok then
-  return
-end
-
--- Have packer use a popup window
---[[ packer.init({ ]]
---[[   display = { ]]
---[[     open_fn = function() ]]
---[[       return require("packer.util").float({ border = "rounded" }) ]]
---[[     end, ]]
---[[   }, ]]
---[[ }) ]]
-return packer.startup(function(use)
-  use("wbthomason/packer.nvim") -- Have packer manage itself
-  use("nvim-lua/popup.nvim") -- An implementation of the Popup API from vim in Neovim
-  use("nvim-lua/plenary.nvim") -- Useful lua functions used ny lots of plugins
-  use("EdenEast/nightfox.nvim") -- color scheme
-  use("windwp/nvim-autopairs") -- Autopairs, integrates with both cmp and treesitter
-  use("numToStr/Comment.nvim") -- Easily comment stuff
-  use("nvim-tree/nvim-web-devicons")
-  use("nvim-tree/nvim-tree.lua")
-  use("lukas-reineke/indent-blankline.nvim")
-  use("folke/which-key.nvim")
-  use("akinsho/toggleterm.nvim")
-  use("SmiteshP/nvim-gps")
-  use("akinsho/bufferline.nvim")
-  use("moll/vim-bbye") -- close buffer command when you use bufferline
-  use("nvim-lualine/lualine.nvim")
-  use("wsdjeg/vim-fetch") -- open file with specify line
-  use("lewis6991/impatient.nvim") -- speed up startup time
-  use("ThePrimeagen/harpoon")
-  use({
-    "phaazon/hop.nvim",
-    branch = "v1", -- optional but strongly recommended
-  })
+  "nvim-lua/popup.nvim", -- An implementation of the Popup API from vim in Neovim
+  "nvim-lua/plenary.nvim", -- Useful lua functions used ny lots of plugins
+  "EdenEast/nightfox.nvim", -- color scheme
+  "windwp/nvim-autopairs", -- Autopairs, integrates with both cmp and treesitter
+  "numToStr/Comment.nvim", -- Easily comment stuff
+  "nvim-tree/nvim-web-devicons",
+  "nvim-tree/nvim-tree.lua",
+  "lukas-reineke/indent-blankline.nvim",
+  "folke/which-key.nvim",
+  "akinsho/toggleterm.nvim",
+  "SmiteshP/nvim-gps",
+  "akinsho/bufferline.nvim",
+  "moll/vim-bbye", -- close buffer command when you use bufferline
+  "nvim-lualine/lualine.nvim",
+  "wsdjeg/vim-fetch", -- open file with specify line
+  "ThePrimeagen/harpoon",
 
   -- LSP
-  use("github/copilot.vim")
-  use({
+  "github/copilot.vim",
+  {
     "VonHeikemen/lsp-zero.nvim",
-    requires = {
+    dependencies = {
       -- LSP Support
       { "neovim/nvim-lspconfig" },
       { "williamboman/mason.nvim" },
@@ -88,39 +57,36 @@ return packer.startup(function(use)
       { "jose-elias-alvarez/null-ls.nvim" },
       { "jayp0521/mason-null-ls.nvim" },
     },
-  })
+  },
 
   -- fuzzy find
-  use({
+  {
     "junegunn/fzf",
-    run = "./install --bin",
-  })
-  use("ibhagwan/fzf-lua")
-  use("airblade/vim-rooter") -- will find .gitignore as project root folder to use fuzzy search
+    build = "./install --bin",
+  },
+  "ibhagwan/fzf-lua",
+  "airblade/vim-rooter", -- will find .gitignore as project root folder to use fuzzy search
 
   -- treesitter
-  use({
+  {
     "nvim-treesitter/nvim-treesitter",
-    run = ":TSUpdate",
-  })
-  use("JoosepAlviste/nvim-ts-context-commentstring")
+    build = ":TSUpdate",
+  },
+  "JoosepAlviste/nvim-ts-context-commentstring",
 
   -- git
-  use("lewis6991/gitsigns.nvim") -- use to display line change on the left of line numbers
+  "lewis6991/gitsigns.nvim", -- use to display line change on the left of line numbers
 
   -- DAP
-  use("mfussenegger/nvim-dap")
-  use("theHamsta/nvim-dap-virtual-text")
-  use("rcarriga/nvim-dap-ui")
-  use("mfussenegger/nvim-dap-python")
-  use("leoluz/nvim-dap-go")
+  "mfussenegger/nvim-dap",
+  "theHamsta/nvim-dap-virtual-text",
+  "rcarriga/nvim-dap-ui",
+  "mfussenegger/nvim-dap-python",
+  "leoluz/nvim-dap-go",
 
   -- Refactor
-  use("ThePrimeagen/refactoring.nvim")
+  "ThePrimeagen/refactoring.nvim",
+}
+local opts = {}
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if PACKER_BOOTSTRAP then
-    require("packer").sync()
-  end
-end)
+require("lazy").setup(plugins, opts)
