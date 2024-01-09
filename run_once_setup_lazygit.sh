b40d62898ea49b377f3929179a077deb7a56d794
jason9075 <jason9075@users.noreply.github.com>
2024-01-09 19:36:19 +0800

    add support for raspberry

--- run_once_setup_lazygit.sh
+++ run_once_setup_lazygit.sh
@@ -5,4 +5,13 @@ if type lazygit &> /dev/null; then
   exit 0
 fi
 
+# if this is rasbian, ignore insatll
+if [ -f /etc/os-release ]; then
+  . /etc/os-release
+  if [ "$ID" = "raspbian" ]; then
+    echo 'raspbian detected, skip installation.'
+    exit 0
+  fi
+fi
+
 "$HOME/.go/bin/go" install github.com/jesseduffield/lazygit@latest
