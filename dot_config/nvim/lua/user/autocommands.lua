1dd13b9b3161f4b49cfceb8516089e47635841d9
jason9075 <jason9075@users.noreply.github.com>
2024-01-10 14:28:26 +0800

    update old vim support

--- dot_config/nvim/lua/user/autocommands.lua
+++ dot_config/nvim/lua/user/autocommands.lua
@@ -1,5 +1,14 @@
-vim.api.nvim_create_autocmd({ "TextYankPost" }, {
-  callback = function()
-    vim.highlight.on_yank { higroup = "Visual", timeout = 200 }
-  end,
+-- Highlight yanked text
+vim.api.nvim_create_autocmd("TextYankPost", {
+    callback = function()
+        vim.highlight.on_yank({ higroup = "Visual", timeout = 200 })
+    end,
+})
+
+-- Chezmoi apply after saving any file in ~/.local/share/chezmoi
+vim.api.nvim_create_autocmd("BufWritePost", {
+    pattern = "*/.local/share/chezmoi/*",
+    callback = function()
+        vim.cmd("silent !chezmoi apply --source-path " .. vim.fn.expand("%:p"))
+    end,
 })
