f70d4712e9f9489dd43781eea4ecafd2a49cb44e
jason9075 <jason9075@users.noreply.github.com>
2024-01-11 15:52:30 +0800

    update git setting and func.zsh

--- dot_config/zsh/func.zsh
+++ dot_config/zsh/func.zsh
@@ -1,11 +1,63 @@
+# Git History Code Browser
+# $1 is the file name, optional
+ghc(){
+  if [ -z "$1" ]; then
+    ghc-all
+  else
+    ghc-file "$1"
+  fi 
+
+}
+ghc-all(){
+  git log --pretty=format:'%h' | \
+  while read -r commit; do
+      git show "$commit" --format="%b" | rg "^[\+\-]" | sed "s/^/$commit /"
+  done | \
+  fzf --ansi \
+      --no-sort \
+      --preview-window 'right:60%' \
+      --preview "echo {} | cut -d' ' -f1 | xargs -I % git show % --color=always | delta" \
+      --bind 'alt-n:preview-down,alt-p:preview-up,alt-s:toggle-sort' \
+      --bind "enter:execute(echo {} | cut -d' ' -f1 | xargs -I % git show % | delta -s)"
+}
+# don't know why, using pipe delta in single file may cause some problem, so I use tput cols to get the width of the terminal
+ghc-file(){
+  local file=$1
+  if [ ! -f "$file" ]; then
+    echo "File "$file" not found!"
+    return 1
+  fi
+
+  git log --pretty=format:'%h' -- $file | \
+  while read -r commit; do
+      git show "$commit" --format="%b" $file | rg "^[\+\-]" | sed "s/^/$commit /"
+  done | \
+  fzf --ansi \
+      --no-sort \
+      --preview-window 'right:60%' \
+      --preview "echo {} | cut -d' ' -f1 | xargs -I % git show % -- $file | delta" \
+      --bind 'alt-n:preview-down,alt-p:preview-up,alt-s:toggle-sort' \
+      --bind "enter:execute(echo {} | cut -d' ' -f1 | xargs -I % git show --color=always % -- $file | delta -s -w $(tput cols) | less -R)" \
+      --bind "ctrl-e:execute(echo {} | cut -d' ' -f1 | xargs -I % git show % | delta -s)"
+}
+
 # Vim Grep
 # ref: https://news.ycombinator.com/item?id=31010090
 # ref: https://www.mankier.com/1/fzf#Key/Event_Bindings
 vg() {
-  rg . --line-number --no-heading --color=always --colors 'match:fg:white' --colors 'match:style:nobold' --smart-case "$@" | \
-    fzf --reverse -d ':' -n 3.. --ansi --preview-window 'right:45%' \
-    --preview 'bat --style=numbers --color=always --highlight-line {2} {1}' \
-    --bind 'enter:become(nvim {1}:{2})'
+  rg . \
+    --line-number \
+    --no-heading \
+    --color=always \
+    --colors 'match:fg:white' \
+    --colors 'match:style:nobold' \
+    --smart-case "$@" | \
+      fzf --reverse -d ':' -n 3.. \
+        --ansi \
+        --preview-window 'right:45%' \
+        --preview 'bat --style=numbers --color=always --highlight-line {2} {1}' \
+        --bind 'alt-n:preview-down,alt-p:preview-up,alt-s:toggle-sort' \
+        --bind 'enter:become(nvim {1}:{2})'
 }
 
 # Git Stash
@@ -32,33 +84,12 @@ gstash() {
 # Git Commit Browser
 # https://gist.github.com/junegunn/f4fca918e937e6bf5bad
 fshow() {
-  local out shas sha q k
-  while out=$(
-      git log --graph --color=always \
-          --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
-      fzf --ansi --multi --no-sort --reverse --query="$q" \
-          --print-query --expect=ctrl-d --toggle-sort=\`); do
-    q=$(head -1 <<< "$out")
-    k=$(head -2 <<< "$out" | tail -1)
-    shas=$(sed '1,2d;s/^[^a-z0-9]*//;/^$/d' <<< "$out" | awk '{print $1}')
-    [ -z "$shas" ] && continue
-    if [ "$k" = ctrl-d ]; then
-      git diff --color=always $shas | delta
-    else
-      for sha in $shas; do
-        git show --color=always $sha | delta
-      done
-    fi
-  done
-}
-
-# Git Blame Browser
-# $1 is the file name
-gbb(){
-  git log --pretty=format:'%h' -- $1 | \  # find all commits that touched the file
-  while read -r commit; do
-      git show "$commit" --format="%H %b" $1 | sed "s/^/$commit /"  # show the commit and diff
-  done | \
-  fzf --ansi --preview 'echo {} | cut -d" " -f1 | xargs -I % git show % --color=always | delta' --preview-window=right:60% \
-    --bind 'alt-n:preview-down,alt-p:preview-up' 
+  git log --graph --color=always \
+      --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
+  fzf --ansi --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort \
+      --bind "ctrl-m:execute:
+                (grep -o '[a-f0-9]\{7\}' | head -1 |
+                xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
+                {}
+FZF-EOF"
 }
--- run_once_setup_git.sh
+++ run_once_setup_git.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-# check git name and email
-if git config --global user.name &> /dev/null; then
-  echo '\033[1;30mgit has been configured, skip configuration.\033[0m'
-  exit 0
+if ! git config --global user.email &> /dev/null; then    
+    echo "Setting up git"
+    GIT_NAME=${GIT_NAME:-"jason9075"}
+    GIT_MAIL=${GIT_MAIL:-"jason9075@users.noreply.github.com"}
+
+    git config --global user.name ${GIT_NAME}
+    git config --global user.email ${GIT_MAIL}
+    git config --global core.editor nvim
+    git config --global init.defaultBranch master
 fi
-
-GIT_NAME=${GIT_NAME:-"jason9075"}
-GIT_MAIL=${GIT_MAIL:-"jason9075@users.noreply.github.com"}
-
-git config --global user.name ${GIT_NAME}
-git config --global user.email ${GIT_MAIL}
-git config --global core.editor nvim
-git config --global init.defaultBranch master
 
 # setup delta
 git config --global include.path ~/.config/delta/themes.gitconfig
@@ -21,3 +18,14 @@ git config --global pager.log delta
 git config --global pager.reflog delta
 git config --global pager.show delta
 
+git config --global interactive.diffFilter "delta --color-only"
+
+git config --global delta.features mellow-barbet
+git config --global delta.side-by-side false
+git config --global delta.hunk-header-style "file syntax"
+git config --global delta.hunk-header-decoration-style "blue ul"
+git config --global delta.plus-emph-style "brightgreen bold italic black"
+git config --global delta.plus-style "green black"
+git config --global delta.minus-emph-style "red bold italic black"
+git config --global delta.minus-style "red black"
+git config --global delta.zero-style "gray"
